3f51bdf896dfbdc7a38146deb4bb5adbafa89c14
slainsama <slainnico@gmail.com>
2024-01-20 15:09:26 +0800

    merge:init and main

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"github.com/fvbock/endless"
-	"github.com/slainsama/msgr_server/inits"
+	"github.com/slainsama/msgr_server/botController"
+	"github.com/slainsama/msgr_server/globals"
+	"github.com/slainsama/msgr_server/scriptController"
 	"github.com/slainsama/msgr_server/server"
 	"log"
 )
 
+func init() {
+	globals.Init()
+	server.Init()
+	botController.Init()
+	scriptController.Init()
+}
+
 func main() {
-	inits.Init()
 	endlessServer := endless.NewServer("0.0.0.0:8081", server.Server)
 	err := endlessServer.ListenAndServe()
 	if err != nil {
