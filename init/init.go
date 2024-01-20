ec455e7c8227a9eaed8ca5d8ae7d378ba33dd152
slainsama <slainnico@gmail.com>
2024-01-20 14:56:32 +0800

    add:init

--- init/init.go
+++ init/init.go
@@ -4,10 +4,12 @@ import (
 	"github.com/slainsama/msgr_server/botController"
 	"github.com/slainsama/msgr_server/globals"
 	"github.com/slainsama/msgr_server/scriptController"
+	"github.com/slainsama/msgr_server/server"
 )
 
 func Init() {
 	globals.Init()
+	server.Init()
 	botController.Init()
 	scriptController.Init()
 }
