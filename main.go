e6904a4fde3bf27226d3180e41a8191f3a3d6441
slainsama <slainnico@gmail.com>
2024-01-20 15:05:51 +0800

    add:init

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/fvbock/endless"
-	"github.com/slainsama/msgr_server/init"
+	"github.com/slainsama/msgr_server/inits"
 	"github.com/slainsama/msgr_server/server"
 	"log"
 )
 
 func main() {
-	init.Init()
+	inits.Init()
 	endlessServer := endless.NewServer("0.0.0.0:8081", server.Server)
 	err := endlessServer.ListenAndServe()
 	if err != nil {
