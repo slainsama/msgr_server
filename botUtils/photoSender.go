39d3fba35c4ddbdb4f0cb9254c91141874566678
node189 <node189@icloud.com>
2024-01-21 10:08:38 +0800

    feat: add photo sender

---
+++ botUtils/photoSender.go
@@ -0,0 +1,27 @@
+package botUtils
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/slainsama/msgr_server/globals"
+	"github.com/slainsama/msgr_server/utils"
+)
+
+func SendPhotoMessage(chatID, caption string, photo []byte) {
+	config := globals.UnmarshaledConfig
+
+	params := map[string]any{
+		"chat_id":    chatID,
+		"caption":    caption,
+		"parse_mode": "MarkdownV2", // Send as markdown text
+		"photo":      photo,
+	}
+	url := config.Bot.APIUrl + config.Bot.Token + config.Bot.Methods.SendPhoto
+	code, body, err := utils.HttpPOST(url, params)
+	if err != nil || code != http.StatusOK {
+		log.Println("Error sending PHOTO message:", err)
+		log.Println("Response Body:", string(body))
+		return
+	}
+}
