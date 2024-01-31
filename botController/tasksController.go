454f8785fcd3db9078f44ec9640bfad0ac631557
slainsama <slainnico@gmail.com>
2024-01-31 21:02:44 +0800

    feat:botControllers

--- botController/tasksController.go
+++ botController/tasksController.go
@@ -1,1 +1,18 @@
 package botController
+
+import (
+	"github.com/slainsama/msgr_server/botUtils"
+	"github.com/slainsama/msgr_server/globals"
+	"github.com/slainsama/msgr_server/models"
+)
+
+func taskController(newUpdate models.TelegramUpdate) {
+	userId := newUpdate.Message.Chat.ID
+	var message string
+	for _, task := range globals.TaskList {
+		if task.UserId == userId {
+			message = message + task.Id + " " + task.ScriptName
+		}
+	}
+	botUtils.SendTextMessage(userId, message)
+}
