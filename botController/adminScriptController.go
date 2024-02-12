f02dbd52589bde73b889f90a1f575c614bdbb04c
slainsama <slainnico@gmail.com>
2024-02-12 14:52:22 +0800

    feat:readme

--- botController/adminScriptController.go
+++ botController/adminScriptController.go
@@ -1,1 +1,7 @@
 package botController
+
+import "github.com/slainsama/msgr_server/models"
+
+func adminAddScriptController(newHandleUpdate models.HandleUpdate) {
+
+}
---
+++ utils/extractCommand.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"github.com/slainsama/msgr_server/models"
+	"strings"
+)
+
+func ExtractCommands(newUpdate models.TelegramUpdate) ([]string, map[string][]string) {
+	var commands []models.Entity
+	var messages []string
+	var startPos int
+	var endPos int
+	argsPos := make(map[string][2]int)
+	args := make(map[string][]string)
+	for _, entity := range newUpdate.Message.Entities {
+		if entity.Type == "bot_command" {
+			commands = append(commands, entity)
+		}
+	}
+	text := newUpdate.Message.Text
+	for _, command := range commands {
+		startPos = command.Offset
+		endPos = startPos + command.Length
+		argsPos[text[startPos:endPos]] = [2]int{startPos, endPos}
+		messages = append(messages, text[startPos:endPos])
+	}
+	messageSize := len(messages)
+	for messagePos, message := range messages {
+		var argsEndPos int
+		argsStartPos := argsPos[message][1]
+		if messagePos > messageSize {
+			argsEndPos = len(newUpdate.Message.Text) - 1
+		} else {
+			argsEndPos = argsPos[messages[messagePos+1]][0]
+		}
+		argString := newUpdate.Message.Text[argsStartPos:argsEndPos]
+		argsResult := strings.Fields(argString)
+		args[message] = argsResult
+	}
+	return messages, args
+}
