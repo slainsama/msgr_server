68dd7c9074680e262e6e9d5cf5f6b2ef37f5c9ab
slainsama <slainnico@gmail.com>
2024-02-15 10:54:40 +0800

    fix:script gorm model

--- models/script.go
+++ models/script.go
@@ -2,10 +2,8 @@ package models
 
 // Script "{scriptName}" to get scriptName;{arg1} to get args
 type Script struct {
-	Id            string
-	Name          string
-	Command       string //such as "python3 {scriptName} {arg1} {arg2}"
-	Status        string
-	ParamRequired []string
-	DataReturn    []string
+	Id      string
+	Name    string
+	Command string //such as "python3 {scriptName} {arg1} {arg2}"
+	Status  string
 }
