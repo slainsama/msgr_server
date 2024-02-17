0fcf22c3108963e7aa6fcaa24ea52bfa38f75129
node189 <node189@icloud.com>
2024-02-17 17:08:08 +0800

    feat: add update dispatcher

--- bot/handler/handler.go
+++ bot/handler/handler.go
@@ -8,9 +8,3 @@ type Handler interface {
 	ShouldHandle(u *models.TelegramUpdate) bool
 	HandlerFunc(u *models.TelegramUpdate)
 }
-
-var Handlers []Handler
-
-func AddHandler(handler Handler) {
-	Handlers = append(Handlers, handler)
-}
